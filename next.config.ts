7ffe1ab3eda080e6584ee23579b5b1d55899647e
tanzimul3islam <tanzimul3islam@gmail.com>
2026-06-03 10:01:23 -0400

    fix: dependency issue

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactCompiler: true,
-  serverExternalPackages: ['kysely'],
+  serverExternalPackages: [
+    'kysely',
+    '@better-auth/kysely-adapter',
+    'better-sqlite3',
+  ],
 };
 
 export default nextConfig;
